12b112958e8503ffc8f1af29fd17ee56ef853df6
Ivan Goncharov <ivan.goncharov@rcntec.com>
2014-10-21 23:32:21 +0400

    Fixes

--- lib/amazon-associates/types/image_set.rb
+++ lib/amazon-associates/types/image_set.rb
@@ -7,6 +7,10 @@ module Amazon
       xml_reader :large, :as => Image, :from => 'LargeImage'
       xml_reader :swatch, :as => Image, :from => 'SwatchImage'
       xml_reader :hi_res_image, :as => Image, :from => 'HiResImage'
+
+      def largest_image_url
+        self.send([:hi_res_image, :large, :medium, :small, :swatch].find{|f| self.try(f) }).url
+      end
     end
   end
 end
