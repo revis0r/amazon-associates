862d347f52fce18dc958b9de5a74d1966d608fde
Ivan Goncharov <revis0r.mob@gmail.com>
2015-11-29 15:21:24 +0300

    Fix

--- lib/amazon-associates/types/item_dimensions.rb
+++ lib/amazon-associates/types/item_dimensions.rb
@@ -1,17 +1,17 @@
 module Amazon
   module Associates
     class ItemDimensions < ApiResult
-      xml_reader :height, :as => Integer, :from => 'Height'
-      xml_reader :length, :as => Integer, :from => 'Length'
-      xml_reader :weight, :as => Integer, :from => 'Weight'
-      xml_reader :width, :as => Integer, :from => 'Width'
+      xml_reader :height, :as => Measurement
+      xml_reader :length, :as => Measurement
+      xml_reader :weight, :as => Measurement
+      xml_reader :width, :as => Measurement
 
       def formatted_weight
-        "#{self.weight.to_i / 100.0} pounds"
+        "#{self.weight.value} #{self.weight.units}"
       end
 
       def formatted_dimensions
-        "#{self.length.to_i / 100.0} x #{self.width.to_i / 100.0} x #{self.height.to_i / 100.0} inches"
+        "#{self.length.value} x #{self.width.value} x #{self.height.value} #{self.length.units}"
       end
     end
   end
